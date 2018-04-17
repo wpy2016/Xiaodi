a5880b7aa1bba89f27aa02afed026605f64abb86
Peiyu.wang <3285840393@qq.com>
2018-04-17 22:41:52 +0800

    在代送记录中增加了未完成订单的联系他的功能

--- app/src/main/java/com/wpy/cqu/xiaodi/reward/AcRewardDetailCanNotCarry.java
+++ app/src/main/java/com/wpy/cqu/xiaodi/reward/AcRewardDetailCanNotCarry.java
@@ -8,6 +8,10 @@ import android.view.View;
 import com.wpy.cqu.xiaodi.R;
 import com.wpy.cqu.xiaodi.application.XiaodiApplication;
 import com.wpy.cqu.xiaodi.evaluate.AcEvaluate;
+import com.wpy.cqu.xiaodi.model.Reward;
+import com.wpy.cqu.xiaodi.util.ToastUtil;
+
+import io.rong.imkit.RongIM;
 
 /**
  * 更新状态的
@@ -18,21 +22,40 @@ public class AcRewardDetailCanNotCarry extends AcRewardDetail {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mtvBack.setText(getResources().getString(R.string.carry_recode));
-
-        mbtnArray.setOnClickListener(this::evaluate);
         mbtnArray.setVisibility(View.GONE);
-        if (reward.Publisher.Id.equals(XiaodiApplication.mCurrentUser.Id)) {
-            if (0 == reward.receiveGrade) {
-                mbtnArray.setVisibility(View.VISIBLE);
-                mbtnArray.setText(getResources().getString(R.string.evaluate));
-            }
-        }
 
-        if (reward.Receiver.Id.equals(XiaodiApplication.mCurrentUser.Id)) {
-            if (0 == reward.publisherGrade) {
-                mbtnArray.setVisibility(View.VISIBLE);
-                mbtnArray.setText(getResources().getString(R.string.evaluate));
-            }
+        if (reward.Publisher.Id.equals(XiaodiApplication.mCurrentUser.Id)) {
+            mySend();
+            return;
+        }
+        myCarry();
+    }
+
+    private void myCarry() {
+        if (Reward.REWARD_STATE_FINISH != reward.state) {
+            mbtnArray.setVisibility(View.VISIBLE);
+            mbtnArray.setText(getResources().getString(R.string.contact_ta));
+            mbtnArray.setOnClickListener(contactTa(reward.Publisher.Id, reward.Publisher.NickName));
+            return;
+        }
+        if (0 == reward.publisherGrade) {
+            mbtnArray.setVisibility(View.VISIBLE);
+            mbtnArray.setText(getResources().getString(R.string.evaluate));
+            mbtnArray.setOnClickListener(this::evaluate);
+        }
+    }
+
+    private void mySend() {
+        if (Reward.REWARD_STATE_FINISH != reward.state) {
+            mbtnArray.setVisibility(View.VISIBLE);
+            mbtnArray.setText(getResources().getString(R.string.contact_ta));
+            mbtnArray.setOnClickListener(contactTa(reward.Receiver.Id, reward.Receiver.NickName));
+            return;
+        }
+        if (0 == reward.receiveGrade) {
+            mbtnArray.setVisibility(View.VISIBLE);
+            mbtnArray.setText(getResources().getString(R.string.evaluate));
+            mbtnArray.setOnClickListener(this::evaluate);
         }
     }
 
@@ -40,5 +63,15 @@ public class AcRewardDetailCanNotCarry extends AcRewardDetail {
         Intent intent = new Intent(this, AcEvaluate.class);
         intent.putExtra("reward", reward);
         startActivity(intent);
+    }
+
+    private View.OnClickListener contactTa(String userId, String title) {
+        return view -> {
+            if (null == RongIM.getInstance()) {
+                ToastUtil.toast(AcRewardDetailCanNotCarry.this, getResources().getString(R.string.rong_yun_not_connect));
+                return;
+            }
+            RongIM.getInstance().startPrivateChat(AcRewardDetailCanNotCarry.this, userId, title);
+        };
     }
 }
